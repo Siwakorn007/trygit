a8013d625d16d8f7f34e4684a55097e0ed794cd2
FisherMan0000 <fish08887@gmail.com>
2024-09-03 14:45:33 +0700

    pai test server

--- server.js
+++ server.js
@@ -4,35 +4,42 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 // const petrolRoutes = require('./routes/petrol');
-// const userRoutes = require('./routes/user');
-const registerRoutes = require('./routes/register');
-const testRoutes = require('./routes/test');
-const register_masterRoutes = require('./routes/register_master');
-const masterRoutes = require('./routes/master');
+const userRoutes = require('./routes/user');  // log for user
+const registerRoutes = require('./routes/register');  //for register user
+const testRoutes = require('./routes/test'); //for testing
+const register_masterRoutes = require('./routes/register_master'); //for register master account
+const masterRoutes = require('./routes/master'); //forlogin master account
+const master_userRoutes = require('./routes/master_user'); //for get all user of master account
+const edit_master_userRoutes = require('./routes/edit_master_user');
 // const paymetRoutes = require('./routes/payment');
 require('dotenv').config();
-// const { readdirSync } = require('fs');
+// const { readdirSync } = require('fs'); //testing readdirSync 
 
 const port = 3001;
-app.use(cors());
+
 
 // app.use(cors({
 //     origin: 'http://localhost:4200' // Angular app's URL
 //   }));
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 // app.use('/api', petrolRoutes); 
-// app.use('/api', userRoutes); 
+app.use('/api', userRoutes); 
 app.use('/api', registerRoutes);
 app.use('/api', testRoutes);
 app.use('/api', register_masterRoutes);
 app.use('/api', masterRoutes);
+app.use('/api', master_userRoutes);
+app.use('/api', edit_master_userRoutes);
+
 // app.use('/api', paymetRoutes);
 
-//===================================//
+//========================testing readdirSync==========================//
 // readdirSync('./routes')
 //  .map((r)=> app.use('/api',require('./routes/' + r)))
 
